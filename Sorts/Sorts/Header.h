c30a95f0ae9862f89a0206e89c8c5d58446a729e
Rei.N <kiba.org@yandex.ru>
2017-06-23 08:18:07 +0900

    1547

--- Sorts/Sorts/Header.h
+++ Sorts/Sorts/Header.h
@@ -10,12 +10,21 @@ public:
 	bool asc;
 	Sorts(int arrr[], bool asc, int n) : asc(asc), arr(arrr), n(n) {};
 	void printSort();
-	void sortArr();
+	virtual void sortArr() = 0;
 };
 
 class Bubble : public Sorts 
 {
 public:
 	Bubble(int aa[], bool asc, int n) : Sorts(aa, asc, n) {};
+	void sortArr();
+};
+
+class Quick : public Sorts
+{
+private:
+	int left, right;
+public:
+	Quick(int aa[], bool asc, int n) : Sorts(aa, asc, n) {};
 	void sortArr();
 };
--- Sorts/Sorts/Source.cpp
+++ Sorts/Sorts/Source.cpp
@@ -1,72 +1,107 @@
-#include "Header.h";
+//#include "Header.h";
+//
+//
+//void Bubble::sortArr() {
+//	int temp;
+//	bool nsorted = true;
+//	if (asc == true) {
+//		while (nsorted) {
+//			nsorted = false;
+//			for (int i = 0; i < n - 1; i++) {
+//				if (arr[i] > arr[i + 1]) {
+//					temp = arr[i + 1];
+//					arr[i + 1] = arr[i];
+//					arr[i] = temp;
+//					nsorted = true;
+//				}
+//			}
+//		}
+//	}
+//	else {
+//		while (nsorted) {
+//			nsorted = false;
+//			for (int i = 0; i < n - 1; i++) {
+//				if (arr[i] < arr[i + 1]) {
+//					temp = arr[i + 1];
+//					arr[i + 1] = arr[i];
+//					arr[i] = temp;
+//					nsorted = true;
+//				}
+//			}
+//		}
+//	}
+//};
+//
+//void Sorts::printSort() {
+//	for (int i = 0; i < n; i++) {
+//		cout << arr[i]<<" ";
+//	}
+//}
+//
+//int main()
+//{
+//	string str, updown;
+//	bool asc = false;
+//	int *arr;
+//	int n;
+//	cout << "yes/no\n";
+//	getline(cin, str);
+//	cout << "asc/desc\n";
+//	getline(cin, updown);
+//	if (str == "yes") {
+//		cin >> n;
+//		arr = new int[n];
+//		for (int i = 0; i < n; i++) {
+//			cin >> arr[i];
+//		}
+//	}
+//	else {
+//		int temp[10] = { 4, 1, 3, 2, 10, 8, 9, 7, 5, 6 };
+//		arr = new int[10];
+//		arr = temp;
+//		n = 10;
+//	}
+//	if (updown == "asc") {
+//		asc = true;
+//	}
+//	Bubble bubsort(arr, asc, n);
+//	bubsort.sortArr();
+//	bubsort.printSort();
+//	//delete[] arr;
+//	return 0;
+//}
 
+#include <iostream> //1547
+#include <math.h>
+using namespace std;
 
-void Bubble::sortArr() {
-	int temp;
-	bool nsorted = true;
-	if (asc == true) {
-		while (nsorted) {
-			nsorted = false;
-			for (int i = 0; i < n - 1; i++) {
-				if (arr[i] > arr[i + 1]) {
-					temp = arr[i + 1];
-					arr[i + 1] = arr[i];
-					arr[i] = temp;
-					nsorted = true;
-				}
-			}
-		}
-	}
-	else {
-		while (nsorted) {
-			nsorted = false;
-			for (int i = 0; i < n - 1; i++) {
-				if (arr[i] < arr[i + 1]) {
-					temp = arr[i + 1];
-					arr[i + 1] = arr[i];
-					arr[i] = temp;
-					nsorted = true;
-				}
-			}
-		}
-	}
-};
-
-void Sorts::printSort() {
-	for (int i = 0; i < n; i++) {
-		cout << arr[i]<<" ";
-	}
+bool hasFractionalPart(double val) {
+	return val - floor(val) > 0;
 }
 
-int main()
-{
-	string str, updown;
-	bool asc = false;
-	int *arr;
-	int n;
-	cout << "yes/no\n";
-	getline(cin, str);
-	cout << "asc/desc\n";
-	getline(cin, updown);
-	if (str == "yes") {
-		cin >> n;
-		arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			cin >> arr[i];
+int main() {
+	int m, n, elemsum, begin, end;
+	double avarage;
+	int triger, last, a_count=0;
+	char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+						 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+	cin >> n >> m;
+	elemsum = pow(26, n);
+	avarage = elemsum / m;
+	if (!hasFractionalPart(avarage)) {
+		for (int i = 0; i < m; i++) {
+			begin = i*avarage + 1;
+			end = avarage*(i + 1);
+			triger = end;
+			last = end % 26;
+			while (triger > 1) {
+				triger /= 26;
+				a_count++;
+			}
+			for (int j = 0; j < a_count; j++) {
+				cout << alphabet[begin] << "-" << alphabet[last] << endl;
+			}
 		}
 	}
-	else {
-		int temp[10] = { 4, 1, 3, 2, 10, 8, 9, 7, 5, 6 };
-		arr = new int[10];
-		arr = temp;
-		n = 10;
-	}
-	if (updown == "asc") {
-		asc = true;
-	}
-	Bubble bubsort(arr, asc, n);
-	bubsort.sortArr();
-	bubsort.printSort();
-	delete[] arr;
 	return 0;
 }
